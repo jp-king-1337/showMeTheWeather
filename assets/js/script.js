3f98441424febd2ad1ac5fe27247c10bcbd03e9b
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-15 20:53:13 -0400

    added lat and lon for my city, just to have something to work with for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 // Add the API
 var weatherKey = "930b816eb4fa5606fcbc2d95d44231f2";
-var latitude = "";
-var longitude = "";
+var latitude = 40.0027;
+var longitude = -76.3547;
 var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherKey}`;
 
 
 fetch(url)
-    .then(weatherData)
+    .then(function(weatherData) {
+        console.log("weatherTest: ", weatherData);
+
+
+    })
