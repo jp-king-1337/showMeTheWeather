1e9a61b6f4c019007e48160f06ed10d07bea32ff
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-15 20:45:31 -0400

    Made the url dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // Add the API
-var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid={api_key}"
+var weatherKey = "930b816eb4fa5606fcbc2d95d44231f2";
+var latitude = "";
+var longitude = "";
+var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherKey}`;
+
 
 fetch(url)
-    .then()
+    .then(weatherData)
