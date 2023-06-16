79ad8fce115e64c319854bf9aa69ca42078b8147
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-15 21:14:00 -0400

    added function that console logs weatherData

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,26 @@ var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=
 
 
 fetch(url)
-    .then(function(weatherData) {
+    .then(function (weatherData) {
         console.log("weatherTest: ", weatherData);
 
+        // Placeholder numbers until I get the whole array of 40 or whatever
+        var forecast = weatherData.list.slice(0, 5);
 
-    })
+        forecast.forEach(function (forecast)) {
+            var date = forecastEntry.dt_txt;
+            var icon = forecastEntry.weather[0].icon;
+            var temperature = forecast.main.temp;
+            var windSpeed = forecastEntry.wind.windSpeed;
+            var humidity = forecastEntry.main.humidity;
+
+            console.log("Date: ", date);
+            console.log("icon: ", icon);
+            console.log("temperature: ", temperature);
+            console.log("windSpeed: ", windSpeed);
+            console.log("humidity: ", humidity);
+        }
+
+
+
+    });
