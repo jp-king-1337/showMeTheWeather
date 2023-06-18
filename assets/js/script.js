81269f8f54e7d4fb87aeb239907367749aebb45e
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-18 07:58:53 -0400

    Added variables for elements I will need based on mockup

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,31 +4,47 @@ var latitude = 40.0027;
 var longitude = -76.3547;
 var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherKey}`;
 
+// Variables for necessary elements
+var searchForm = document.getElementById("searchForm");
+var cityInput = document.getElementById("cityInput");
+var cityNameEl = document.getElementById("cityName");
+var dateEl = document.getElementById("date");
+var weatherIconEl = document.getElementById("weatherIcon");
+var temperatureEl = document.getElementById("temperature");
+var humidityEl = document.getElementById("humidity");
+var windSpeedEl = document.getElementById("windSpeed");
+// Also container to hold forecast
+var forecastEl = document.getElementById("forecast");
 
-fetch(url)
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (weatherData) {
-        console.log("weatherTest: ", weatherData);
 
-        // Placeholder numbers until I get the whole array of 40 or whatever
-        var forecast = weatherData.list.slice(0, 5);
+function fetchWeatherData(city) {
+    fetch(url)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (weatherData) {
+            console.log("weatherTest: ", weatherData);
 
-        forecast.forEach(function (forecast) {
-            var date = forecastEntry.dt_txt;
-            var icon = forecastEntry.weather[0].icon;
-            var temperature = forecastEntry.main.temp;
-            var windSpeed = forecastEntry.wind.windSpeed;
-            var humidity = forecastEntry.main.humidity;
+            // Placeholder numbers until I get the whole array of 40 or whatever
+            var forecast = weatherData.list.slice(0, 5);
 
-            console.log("Date: ", date);
-            console.log("Icon: ", icon);
-            console.log("Temperature: ", temperature);
-            console.log("Wind Speed: ", windSpeed);
-            console.log("Humidity: ", humidity);
+            forecast.forEach(function (forecast) {
+                var date = forecastEntry.dt_txt;
+                var icon = forecastEntry.weather[0].icon;
+                var temperature = forecastEntry.main.temp;
+                var windSpeed = forecastEntry.wind.windSpeed;
+                var humidity = forecastEntry.main.humidity;
+
+                console.log("Date: ", date);
+                console.log("Icon: ", icon);
+                console.log("Temperature: ", temperature);
+                console.log("Wind Speed: ", windSpeed);
+                console.log("Humidity: ", humidity);
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
         });
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+}
+
+
