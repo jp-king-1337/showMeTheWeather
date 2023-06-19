7f512eafb2e737106df809e5983105196747afd3
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-18 21:21:16 -0400

    Removed my latitude and longitude. I think I just want the latitude and longitude from the searched city? Or maybe I don't need that at all. Will try and see.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Add the API
 var weatherKey = "930b816eb4fa5606fcbc2d95d44231f2";
-var latitude = 40.0027;
-var longitude = -76.3547;
+// var latitude = 40.0027;
+// var longitude = -76.3547;
 var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherKey}`;
 var forecastUrl;
 
